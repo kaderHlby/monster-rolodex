f0a3b4f78c5e7940a0ee744a74099bcda4a338f4
kader <kader@seekasia.com>
2020-12-27 16:38:17 +0800

    styling and refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
 import './App.css';
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
 class App extends Component {
   constructor() {
     super();
@@ -16,6 +18,10 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
 
+  handelChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(
@@ -23,10 +29,10 @@ class App extends Component {
     )
     return (
       <div className="App">
-        <input
-          type="search"
+        <h1>Monster Rolodex</h1>
+        <SearchBox
           placeholder="search monsters"
-          onChange={e => this.setState({ searchField: e.target.value })}
+          handelChange={this.handelChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
